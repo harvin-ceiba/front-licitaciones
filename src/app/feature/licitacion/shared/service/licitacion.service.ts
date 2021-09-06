b35252ea08609112dd6c36bebbb5e7721c827739
Harvin Rengifo <harvin.rengifo@ceiba.com.co>
2021-09-06 01:57:42 -0500

    Update licitacion.service.ts

--- src/app/feature/licitacion/shared/service/licitacion.service.ts
+++ src/app/feature/licitacion/shared/service/licitacion.service.ts
@@ -8,27 +8,39 @@ export class LicitacionService {
 
   constructor(protected http: HttpService) {}
 
-  public consultar() {
+  public consultarTodos() {
     return this.http.doGet<Licitacion[]>(
       `${environment.endpoint}/licitaciones`, this.http.optsName('Consultar licitaciones')
     );
   }
 
+  public consultarPorId(id: number) {
+    return this.http.doGet<Licitacion>(
+      `${environment.endpoint}/licitaciones/${id}`, this.http.optsName('Consultar una licitación')
+    );
+  }
+
   public guardar(licitacion: Licitacion) {
     return this.http.doPost<Licitacion, any>(
-      `${environment.endpoint}/licitaciones`, licitacion, this.http.optsName('Crear/Actualizar licitaciones')
+      `${environment.endpoint}/licitaciones`, licitacion, this.http.optsName('Crear licitaciones')
     );
   }
 
-  public publicar(licitacion: Licitacion) {
-    return this.http.doPost<Licitacion, any>(
-      `${environment.endpoint}/licitaciones`, licitacion, this.http.optsName('Publicar licitaciones')
+  public editar(licitacion: Licitacion) {
+    return this.http.doPut<Licitacion, any>(
+      `${environment.endpoint}/licitaciones/${licitacion.id}`, licitacion, this.http.optsName('Actualizar licitaciones')
     );
   }
 
-  public eliminar(licitacion: Licitacion) {
+  public eliminar(id: number) {
     return this.http.doDelete<any>(
-      `${environment.endpoint}/licitaciones/${licitacion.id}`, this.http.optsName('Eliminar licitaciones'));
+      `${environment.endpoint}/licitaciones/${id}`, this.http.optsName('Eliminar licitaciones'));
+  }
+
+  public publicar(id: number) {
+    return this.http.doPut<Licitacion, boolean>(
+      `${environment.endpoint}/licitaciones/${id}/publicar`, null, this.http.optsName('Publicar licitaciones')
+    );
   }
 
 }
